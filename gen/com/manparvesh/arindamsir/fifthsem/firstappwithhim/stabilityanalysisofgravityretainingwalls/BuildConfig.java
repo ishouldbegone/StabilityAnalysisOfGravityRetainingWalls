a193fe5042b5051732e382cc050745f34239e8b3
ishouldgo <ishouldbegone@gmail.com>
2014-10-23 15:52:12 +0530

    adding project components
    Signed-off-by: ishouldgo <ishouldbegone@gmail.com>

---
+++ gen/com/manparvesh/arindamsir/fifthsem/firstappwithhim/stabilityanalysisofgravityretainingwalls/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.manparvesh.arindamsir.fifthsem.firstappwithhim.stabilityanalysisofgravityretainingwalls;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
